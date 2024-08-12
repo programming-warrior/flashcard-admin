b20f2df585487959a6950ba66494de2125023cbb
Yusuf <mdyusuf2521@gmail.com>
2024-08-12 11:38:20 +0530

    login credentials

--- src/Login.tsx
+++ src/Login.tsx
@@ -36,6 +36,8 @@ const Login = () => {
 
     return (
         <div className="w-screen h-screen flex flex-col justify-center items-center ">
+            <h3>username:Yusuf</h3>
+            <h3>password:12345</h3>
             <h1 className="text-2xl font-bold mb-6">Login</h1>
             <div ref={errorDiv}></div>
             <Form onSubmit={handleSubmit} onUsernameChange={(e: any) => { setUsername(e.target.value) }} onPasswordChanage={(e: any) => { setPassword(e.target.value) }} />
